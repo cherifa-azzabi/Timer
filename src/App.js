53a1b578ee8c00925a052f606ce5f977ee937002
cherifa-azzabi <azzabiicherifa@gmail.com>
2020-02-24 10:12:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+//state
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: false,
+      runningTime: 0,
+      seconds: 0,
+      hours: 0,
+      minutes: 0
+    };
+    setInterval(() => {
+      if (this.state.status) {
+        this.setState({
+          runningTime: this.state.runningTime + 1
+        });
+        this.timeConversion(this.state.runningTime);
+      }
+    }, 1000);
+  }
+  timeConversion = s => {
+    this.setState({
+      hours: Math.floor(s / 3600),
+      minutes: Math.floor((s - this.state.hours * 3600) / 60),
+      seconds: Math.floor(
+        (s - this.state.hours * 3600 - this.state.minutes * 60) / 1
+      )
+    });
+  };
+  Click = () => {
+    this.setState({ status: !this.state.status });
+  };
+  Reset = () => {
+    this.setState({
+      status: false,
+      runningTime: 0,
+      seconds: 0,
+      hours: 0,
+      minutes: 0
+    });
+  };
+  render() {
+    return (
+      <div className="container">
+        <div className="container">
+          <div>
+            <p>{String(this.state.hours).padStart(2, "0")}:</p>
+            <p> H </p>
+          </div>
+          <div>
+            <p>{String(this.state.minutes).padStart(2, "0")}: </p>
+            <p> Min </p>
+          </div>
+          <div>
+            <p>{String(this.state.seconds).padStart(2, "0")}</p>
+            <p> Sec </p>
+          </div>
+        </div>
+        <div>
+          <button onClick={this.Click}>
+            {this.state.status ? "Stop" : "Start"}
+          </button>
+          <button onClick={this.Reset}>Reset</button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default Timer;
